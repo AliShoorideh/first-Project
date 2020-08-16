b9caeea054f5b019828c0cb72400eb77e2994eca
AliShoorideh <a.shsh78@yahoo.com>
2020-08-17 00:54:47 +0430

    new fucking navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,2 @@
 $(document).ready(function () {
-    $(".fp-shop").mouseenter(function(){
-        $(".fp-dropdown-shop").removeClass("hide");
-      });
-      $(".fp-dropdown-shop").mouseleave(function(){
-        $(this).addClass("hide");
-      });
-    
-      
     });
