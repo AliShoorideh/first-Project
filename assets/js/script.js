1f54c5f143c66d2652f870d7f603e22b4c5e3c77
AliShoorideh <a.shsh78@yahoo.com>
2020-08-18 01:53:10 +0430

    final navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
 $(document).ready(function () {
-    });
+    $('.dropdown').hover(
+        function () {
+            $(this).children('.dropdown-content')
+                .delay(0) // set duration of delay in milliseconds here
+                .slideDown(200);
+        },
+        function () {
+            $(this).children('.dropdown-content')
+                .clearQueue()
+                .slideUp(0);
+        });
+   
+});
